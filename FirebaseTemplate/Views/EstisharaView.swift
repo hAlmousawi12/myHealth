fc44fc53afc0aa670f7a233518f0e23272f1629b
Hussain Almousawi <70634196+hAlmousawi12@users.noreply.github.com>
2022-05-02 22:56:07 +0300

    Done

--- FirebaseTemplate/Views/EstisharaView.swift
+++ FirebaseTemplate/Views/EstisharaView.swift
@@ -11,6 +11,56 @@ import SwiftUI
 struct EstisharaView: View {
     @StateObject var env: FirebaseEnv
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Header(env: env)
+            VStack(alignment: .trailing) {
+                Text("")
+                    .font(.title)
+                    .foregroundColor(.theme.secondary)
+                ScrollView(.horizontal, showsIndicators: false) {
+                    HStack {
+                        Image("1")
+                            .resizable()
+                            .scaledToFill()
+                            .frame(width: 150, height: 200)
+                        Image("2")
+                            .resizable()
+                            .scaledToFill()
+                            .frame(width: 150, height: 200)
+                        Image("3")
+                            .resizable()
+                            .scaledToFill()
+                            .frame(width: 150, height: 200)
+                        Image("4")
+                            .resizable()
+                            .scaledToFill()
+                            .frame(width: 150, height: 200)
+                        Image("5")
+                            .resizable()
+                            .scaledToFill()
+                            .frame(width: 150, height: 200)
+                        Image("6")
+                            .resizable()
+                            .scaledToFill()
+                            .frame(width: 150, height: 200)
+                    }
+                }
+            }
+            VStack(alignment: .trailing) {
+                Text("افضل الدكاترة")
+                    .font(.title)
+                    .foregroundColor(.theme.secondary)
+                Image("Fatima")
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 350, height: 75)
+                Image("Bdr")
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 350, height: 75)
+                    .padding(.vertical)
+            }
+            Spacer()
+        }
     }
 }
