3d0151f78f19252ec42c2ab37850d4f611e216ca
Hussain Almousawi <70634196+hAlmousawi12@users.noreply.github.com>
2022-05-02 22:21:58 +0300

    Cart View Done

--- FirebaseTemplate/Views/CartView.swift
+++ FirebaseTemplate/Views/CartView.swift
@@ -11,6 +11,40 @@ import SwiftUI
 struct CartView: View {
     @StateObject var env: FirebaseEnv
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Header(env: env)
+            Spacer()
+            Image("Group 1812")
+                .resizable()
+                .scaledToFill()
+                .frame(width: 350, height: 75)
+            Spacer()
+            VStack(alignment: .trailing, spacing: 10) {
+                Text("ملخص الدفع")
+                    .font(.title2)
+                HStack {
+                    Text("10،000د.ك")
+                        .font(.title3)
+                    Spacer()
+                    Text("المجموع")
+                        .font(.title3)
+                }
+            }.padding()
+            HStack {
+                Text("تنفيذ الطلب")
+                    .foregroundColor(.white)
+                    .frame(width: 150, height: 50)
+                    .background(Color.red)
+                    .clipShape(Capsule())
+                NavigationLink(destination: PharmacyView(env: env)) {
+                    Text("اضافة المزيد  ")
+                        .foregroundColor(.white)
+                        .frame(width: 150, height: 50)
+                        .background(Color.red)
+                        .clipShape(Capsule())
+                }
+
+            }.padding()
+        }
     }
 }
