134dec0ce635e45162bf12d6252261ccbf2a9e13
Hussain Almousawi <70634196+hAlmousawi12@users.noreply.github.com>
2022-05-02 22:10:41 +0300

    Favorite View Done

--- FirebaseTemplate/Views/FavoriteView.swift
+++ FirebaseTemplate/Views/FavoriteView.swift
@@ -11,6 +11,68 @@ import SwiftUI
 struct FavoriteView: View {
     @StateObject var env: FirebaseEnv
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Header(env: env)
+            Spacer()
+            Text("المفضلة")
+                .font(.largeTitle)
+                .foregroundColor(.theme.secondary)
+            
+            VStack(alignment: .trailing) {
+                Text("الاطباء")
+                    .foregroundColor(.theme.secondary)
+                    .font(.title2)
+                    .fontWeight(.semibold)
+                Doctor(isFav: true, doctorName: "د. تامر عباس", doctorType: "دكتور عيون", color: .blue)
+                
+                Doctor(isFav: true, doctorName: "د. مريم", doctorType: "دكتور عيون", color: .orange)
+                    .padding(.vertical)
+            }.padding()
+            VStack(alignment: .trailing) {
+                Text("الادوية")
+                    .foregroundColor(.theme.secondary)
+                    .font(.title2)
+                    .fontWeight(.semibold)
+                Image("Group 1818")
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 300, height: 75)
+                Image("k")
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 300, height: 75)
+            }.padding()
+            Spacer()
+        }
+    }
+}
+
+struct Doctor: View {
+    var isFav: Bool
+    var doctorName: String
+    var doctorType: String
+    var color: Color
+    var body: some View {
+        HStack {
+            Image(systemName: isFav ? "star.fill" : "star")
+                .foregroundColor(isFav ? .yellow : .black)
+                .font(.title)
+            
+            Spacer()
+            
+            VStack(alignment: .trailing) {
+                Text(doctorName)
+                Text(doctorType)
+                    .font(.caption2)
+            }
+            Circle()
+                .foregroundColor(color)
+                .frame(width: 45, height: 45)
+            
+        }
+        .padding(10)
+        .frame(width: 300, height: 75)
+        .background(color.opacity(0.3))
+        .cornerRadius(10)
     }
 }
