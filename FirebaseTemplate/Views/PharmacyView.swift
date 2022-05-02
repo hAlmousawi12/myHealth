dce50221c28abf0792d2c8d0db7ae55e6bf9e553
Hussain Almousawi <70634196+hAlmousawi12@users.noreply.github.com>
2022-05-02 22:38:18 +0300

    Pharmacy View Done

--- FirebaseTemplate/Views/PharmacyView.swift
+++ FirebaseTemplate/Views/PharmacyView.swift
@@ -10,7 +10,33 @@ import SwiftUI
 
 struct PharmacyView: View {
     @StateObject var env: FirebaseEnv
+    @State var selection: Category = .bestSeller
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Header(env: env)
+            Picker("", selection: $selection) {
+                ForEach(Category.allCases, id: \.self) {
+                    Text($0.rawValue)
+                }
+            }
+            .pickerStyle(SegmentedPickerStyle())
+            .padding(.vertical)
+            Spacer()
+            Image(selection.rawValue)
+                .resizable()
+                .scaledToFit()
+            
+            Spacer()
+        }
     }
+}
+
+
+
+enum Category: String, CaseIterable {
+    case bestSeller = "الاكثر مبيعاً"
+    case medicine = "الادوية"
+    case nose = "بخاخ الانف"
+    case vit = "الفيتامينات"
+    case place = "الموضعية"
 }
